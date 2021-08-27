c3d651a35b43e3077c3e5bbd788201f4b1f389fd
Ala <ala-hmz@outlook.com>
2021-08-27 17:41:06 +0100

    testing poll

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//plz
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
